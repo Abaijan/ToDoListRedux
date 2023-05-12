bb5728f5c3ee865e95e9f9af4da036e74624b4f5
Abaijan <fghf09647@gmail.com>
2023-05-12 17:03:52 +0600

    russkayaaa vodka ji estttt

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,36 @@
 import { useState } from 'react';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { addTodo, delTodo } from './redux/redusers/todo';
-import { AiOutlineDelete } from 'react-icons/ai'
-import { TbBrandRedux} from 'react-icons/tb'
-import s from './styles.css/style.module.css'
+import { delTodo, addTodo, doImportant, doDone, delAll } from './redux/redusers/todo';
 
 function App() {
-  const dispatch = useDispatch();
-  const todos = useSelector((store) => store.todos.todos)
-  const [todo, setTodo] = useState('')
+const dispatch = useDispatch()
+const todos = useSelector((store) => store.todos.todos)
+
+
+const [todo, setTodo] = useState('')
+
+
 
   return (
     <div className="App">
-      <div className={s.container}>
-        <div className={s.inner_todo}>
-          <div className={s.titles}>
-          <h1 className={s.title}>To do List on Redux</h1>
-          <TbBrandRedux className={s.redux_img}/>
-          </div>
-          <form onSubmit={(e) => {
-            e.preventDefault()
-            if (e.target[0].value.trim().length) {
-              dispatch(addTodo(todo))
-              setTodo('')
-            }
-          }} >
-            <input onChange={(e) => setTodo(e.target.value)} value={todo} type="text" placeholder='Add to do' className={s.addTask} />
-            <button className={s.addtask} type='button'
-            >add</button>
-          </form>
-          <div className={s.contain}>
-            {
-              todos.map((item) => (
-                <div key={item.id} className={s.inner}>
-                  <li className={s.todo}>{item.title}</li>
-                  <button className={s.delete_todo} onClick={() => dispatch(delTodo(item.id))}>
-                    <AiOutlineDelete className={s.delete_todoIMg} />
-                  </button>
-                </div>
-              ))
-            }
-          </div>
+        <div>
+          <input onChange={(e) => setTodo(e.target.value)} value={todo} type="text" />
+          <button  type='button' onClick={() => {dispatch(addTodo(todo))
+          setTodo('')
+          }}>Add</button>
+
+          {
+            todos.map((item) => (
+              <li key={item.id} style={{marginLeft: '10px 0', color: item.isImportant ? 'red' : '' , textDecoration : item.isDone ? 'line-through' : '' }}>{item.title}
+              <button style={{margin: '0 50px'}} type='button' onClick={() => dispatch(delTodo(item.id))}>Delete</button>
+              <button type='button' onClick={() => dispatch(doImportant(item.id), setTodo(""))}>Important</button>
+              <button onClick={() => dispatch(doDone(item.id),setTodo(''))} type='button'>Done</button>
+              </li>
+            ))
+          }
+          <button onClick={()=> dispatch(delAll(todos) ,setTodo(''))}>deleteAll</button>
         </div>
-      </div>
     </div>
   );
 }
--- src/redux/redusers/todo.js
+++ src/redux/redusers/todo.js
@@ -1,38 +1,108 @@
+const ADD = 'ADD'
+const DELETE = 'DELETE'
+const DONE = 'DONE'
+const IMPORTANT = ' IMPORTANT'
+const DELETEALL = 'DELETEALL'
+const EDIT = 'EDIT'
+const EDIT2 = 'EDIT2'
+
 
 const initialState = {
-    todos:[]
+    todos: [
+        {
+            id: 1,
+            title: 'value',
+            isDelete: false,
+            isImportant: false,
+            isDone: false,
+            change: false
+        }
+    ],
 }
- const state = ((state = initialState, action) =>{
-    switch(action.type){
-        case 'ADD': {
+const state = ((state = initialState, action) => {
+    switch (action.type) {
+        case ADD: {
             return {
                 ...state,
-                todos: [...state.todos, {title: action.title,
-                id: Date.now()}]
+                todos: [...state.todos, {
+                    title: action.title,
+                    id: state.todos.length ? state.todos[state.todos.length - 1].id + 1 : 1
+                }
+                ]
             }
         }
-        case 'DEL': {
-            return{
+        case DELETE: {
+            return {
                 ...state,
                 todos: state.todos.filter((el) => el.id !== action.id)
             }
         }
-
+        case DONE: {
+            return {
+                ...state,
+                todos: state.todos.map((item) => {
+                    if (item.id == action.id) {
+                        return {
+                            ...item, isDone: !item.isDone
+                        }
+                    }
+                    return item
+                })
+            }
+        }
+        case IMPORTANT: {
+            return {
+                ...state,
+                todos: state.todos.map((item) => {
+                    if (item.id == action.id) {
+                        return {
+                            ...item, isImportant: !item.isImportant
+                        }
+                    }
+                    return item
+                })
+            }
+        }
+        case DELETEALL: {
+            return {
+                 ...state,
+                 todos : []
+                
+            }
+        }
 
         default: {
             return state
         }
     }
 }
- )
- export default state
-export const addTodo = (title) =>{
-    return(dispatch) =>{
-        return dispatch({type: 'ADD', title})
+)
+export default state
+
+
+export const addTodo = (title) => {
+    return (dispatch) => {
+        return dispatch({ type: ADD, title })
     }
 }
-export const delTodo = (id) =>{
-    return(dispatch) =>{
-        return dispatch({type: 'DEL', id})
+export const delTodo = (id) => {
+    return (dispatch) => {
+        return dispatch({ type: DELETE, id })
+    }
+}
+export const doImportant = (id) => {
+    return (dispatch) => {
+        return dispatch({ type: IMPORTANT, id })
+    }
+}
+
+export const doDone = (id) => {
+    return (dispatch) => {
+        return dispatch({ type: DONE, id })
+    }
+}
+export const delAll = (title) => {
+    return (dispatch) => {
+        return dispatch({ type: DELETEALL })
     }
 }
